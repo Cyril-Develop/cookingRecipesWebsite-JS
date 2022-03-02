8f3fa76a83c54bdd7a0fe88845f552993ac8f98b
Cyril-Develop <90571188+Cyril-Develop@users.noreply.github.com>
2022-03-02 23:53:22 +0100

    Add files via upload

--- cookingRecipesWebsite-JS/main.js
+++ cookingRecipesWebsite-JS/main.js
@@ -3,11 +3,13 @@ const blockRecettes = document.querySelectorAll('.block-recette');
 
 inputSearch.addEventListener('keyup', () => {
     let searchedLetters = inputSearch.value;
+    //console.log(searchedLetters);
     correspondance(searchedLetters, blockRecettes);
 })
 
 function correspondance(letters, elements) {
     for(let i = 0; i < elements.length; i++) {  
+        console.log(elements[i].textContent);
         if(elements[i].textContent.toLowerCase().includes(letters)) {
             elements[i].style.display = "block"
         } else {
